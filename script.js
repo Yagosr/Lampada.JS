68b202324ba2e96d656a26ce1d5f21854c711d2b
Yagosr <yagosr25@gmail.com>
2023-04-26 21:58:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const turnOn = document.getElementById ("turnOn")
+const turnOff = document.getElementById ("turnOff")
+const lamp = document.getElementById ("lamp")
+
+function lampOn () {
+    lamp.src = './img/ligada.jpg';
+}
+    
+function lampOff () {
+    lamp.src = './img/desligada.jpg';
+}
+
+function lampBroke () {
+    lamp.src = './img/quebrada.jpg'
+}
+  
+turnOn.addEventListener ('click', lampOn);
+turnOff.addEventListener ('click', lampOff);
+lamp.addEventListener ('dblclick', lampBroke)
